e6e20347d646725da4f1eaa79ca28ce36dae02f0
qwe <qwe@qwe>
2020-09-20 20:47:12 +0800

    no msg

--- InAction/ch2_/app.js
+++ InAction/ch2_/app.js
@@ -4,9 +4,9 @@ const os = require('os');
 console.log("Kubia server starting...");
 
 var handler = function (request, response) {
-	console.log("Received request from " + request.connection.remoteAddress);
-	response.writeHead(200);
-	response.end("You've hit " + os.hostname() + "\n");
+    console.log("Received request from " + request.connection.remoteAddress);
+    response.writeHead(200);
+    response.end("You've hit " + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
